7deaf27ae61f66baf58dc1bab3dc0c6abe7cf0dd
Emrah Burak Kaya <emrhbrkky@outlook.com>
2025-03-23 18:33:36 +0300

    Add project files.

---
+++ IDistributedCacheRedisApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //Redis'i basit versiyonda yani sadece get set işlemleri yapabileceğimiz şekilde kullanabilmek için IDisributedCache'i kullanıyoruz.
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
